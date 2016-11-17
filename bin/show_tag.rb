c240d2e9e61836abfab3913bf507c641979b50b0
Jason Frey <fryguy9@gmail.com>
2016-11-17 14:37:05 -0500

    Add scripts for showing repos and tags.

---
+++ bin/show_tag.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << File.expand_path("../lib", __dir__)
+
+require 'bundler/setup'
+require 'manageiq/release'
+require 'more_core_extensions/core_ext/array/tableize'
+require 'trollop'
+
+opts = Trollop.options do
+  opt :tag,    "The tag name",      :type => :string, :required => true
+  opt :branch, "The target branch", :type => :string, :required => true
+end
+
+HEADER = %w(Repo SHA Message).freeze
+
+def show_tag(repo, tag)
+  line = repo.git.capturing.show({:summary => true, :oneline => true}, tag)
+  sha, message = line.split(" ", 2)
+  [repo.name, sha, message]
+end
+
+repos = ManageIQ::Release::Repos[opts[:branch]]
+table = [HEADER] + repos.collect { |repo| show_tag(repo, opts[:tag]) }
+puts table.tableize(:max_width => 75)
