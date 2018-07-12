22f1173fd9400eb7fed6db38dd8f54af8a8a20d5
Nick LaMuro <nicklamuro@gmail.com>
2018-07-12 17:10:11 -0500

    [fetch_repos] Add --checkout flag
    Optional flag to not only fetch the repos, but to check out the proper
    branch (for when doing a backporting session).
    
    Original functionality is still the default.

--- bin/fetch_repos.rb
+++ bin/fetch_repos.rb
@@ -7,12 +7,16 @@ require 'manageiq/release'
 require 'trollop'
 
 opts = Trollop.options do
-  opt :branch, "The target branch", :type => :string, :required => false
+  opt :branch,   "The target branch",           :type => :string,  :required => false
+  opt :checkout, "Also checkout target branch", :type => :boolean, :default => false
 end
 
 repos = opts[:branch] ? ManageIQ::Release::Repos[opts[:branch]] : ManageIQ::Release::Repos.all_repos
 repos.each do |repo|
   puts ManageIQ::Release.header(repo.name)
   repo.fetch
+  Dir.chdir(repo.path) do
+    repo.checkout(opts[:branch])
+  end if opts[:checkout] && opts[:branch] && !repo.options["has_real_releases"]
   puts
 end
