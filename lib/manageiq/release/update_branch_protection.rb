a5e1ec7a0a5da629bddc44ffd5cc68c4d1019ed9
Jason Frey <fryguy9@gmail.com>
2018-09-25 15:55:43 -0400

    Extract branch protection into a separate module

---
+++ lib/manageiq/release/update_branch_protection.rb
@@ -0,0 +1,40 @@
+module ManageIQ
+  module Release
+    class UpdateBranchProtection
+      attr_reader :repo, :branch, :dry_run
+
+      def initialize(repo, branch:, dry_run: false)
+        @repo    = repo
+        @branch  = branch
+        @dry_run = dry_run
+      end
+
+      def run
+        protect_branch
+      end
+
+      private
+
+      def protect_branch
+        puts "Protecting #{branch} branch"
+
+        settings = {
+          :enforce_admins                => nil,
+          :required_status_checks        => nil,
+          :required_pull_request_reviews => nil,
+          :restrictions                  => nil
+        }
+
+        if dry_run
+          puts "** dry-run: github.protect_branch(#{repo.inspect}, #{branch.inspect}, #{settings.inspect[1..-2]})"
+        else
+          github.protect_branch(repo, branch, settings)
+        end
+      end
+
+      def github
+        ManageIQ::Release.github
+      end
+    end
+  end
+end
