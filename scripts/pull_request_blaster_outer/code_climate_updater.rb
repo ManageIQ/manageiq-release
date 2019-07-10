3eedd0599c173e4d4234c2d48c3d18ce8ab04cc0
Daniel Berger <djberg96@gmail.com>
2019-07-10 11:32:58 -0400

    Just exit if not found.

--- scripts/pull_request_blaster_outer/code_climate_updater.rb
+++ scripts/pull_request_blaster_outer/code_climate_updater.rb
@@ -2,24 +2,27 @@
 require 'yaml'
 
 file = '.codeclimate.yml'
+version = 'rubocop-0-69' # Update as needed
 
-if File.exists?(file)
-  data = YAML.load_file(file)
-  version = 'rubocop-0-69'
-
-  # Most ManageIQ repositories
-  if data['engines'] && data['engines']['rubocop']
-    unless data['engines']['rubocop']['channel'] && data['engines']['rubocop']['channel'] == version
-      data['engines']['rubocop']['channel'] = version
-    end
-  end
-
-  # Toplogical inventory, et al
-  if data['plugins'] && data['plugins']['rubocop']
-    unless data['plugins']['rubocop']['channel'] && data['plugins']['rubocop']['channel'] == version
-      data['plugins']['rubocop']['channel'] = version
-    end
-  end
-
-  File.open(file, 'w'){ |fh| YAML.dump(data, fh) }
+unless File.exist?(file)
+  puts "#{file} doesn't exist, skipping"
+  exit 0
 end
+
+data = YAML.load_file(file)
+
+# Most ManageIQ repositories
+if data['engines'] && data['engines']['rubocop']
+  unless data['engines']['rubocop']['channel'] && data['engines']['rubocop']['channel'] == version
+    data['engines']['rubocop']['channel'] = version
+  end
+end
+
+# Toplogical inventory, et al
+if data['plugins'] && data['plugins']['rubocop']
+  unless data['plugins']['rubocop']['channel'] && data['plugins']['rubocop']['channel'] == version
+    data['plugins']['rubocop']['channel'] = version
+  end
+end
+
+File.open(file, 'w'){ |fh| YAML.dump(data, fh) }
