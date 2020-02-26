1bccfc409cd068280cddb1bffa8ac6e5826132d1
Jason Frey <fryguy9@gmail.com>
2020-02-26 16:12:15 -0500

    Add start/end range to sprint enumerator

--- lib/manageiq/release/sprint_milestone.rb
+++ lib/manageiq/release/sprint_milestone.rb
@@ -15,27 +15,34 @@ module ManageIQ
       end
 
       def self.next_title
-        build_title(*sprints.first)
+        number, _start_date, end_date = sprints.first
+        build_title(number, end_date)
       end
 
-      def self.build_title(number, date)
-        "Sprint #{number} Ending #{date.strftime("%b %-d, %Y")}"
+      def self.build_title(number, end_date)
+        "Sprint #{number} Ending #{end_date.strftime("%b %-d, %Y")}"
       end
 
       def self.sprints(as_of: Date.today)
+        as_of ||= Date.new(0)
+
         require "active_support/core_ext/numeric/time"
         # The first date when we started doing this cadence
-        number = 76
-        date   = Date.parse("Jan 1, 2018")
+        number     = 76
+        start_date = Date.parse("Dec 12, 2017")
+        end_date   = Date.parse("Jan 1, 2018")
 
         Enumerator.new do |y|
           loop do
-            y << [number, date] if date >= as_of
+            y << [number, start_date, end_date] if end_date >= as_of
 
             number += 1
-            date   += 2.weeks
-            while (date.month == 12 && (22..31).cover?(date.day)) || (date.month == 1 && (1..4).cover?(date.day))
-              date += 1.weeks
+            start_date = end_date + 1.day
+
+            end_date += 2.weeks
+            while (end_date.month == 12 && (22..31).cover?(end_date.day)) ||
+                  (end_date.month == 1 && (1..4).cover?(end_date.day))
+              end_date += 1.weeks
             end
           end
         end
