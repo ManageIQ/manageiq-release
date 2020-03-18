f69976309193d40787342b1cc3b3d53c153a9fd6
Jason Frey <fryguy9@gmail.com>
2020-03-18 15:53:40 -0400

    Add support for release milestone due date

--- bin/release_milestone.rb
+++ bin/release_milestone.rb
@@ -7,11 +7,16 @@ require 'manageiq/release'
 require 'optimist'
 
 opts = Optimist.options do
-  opt :repo, "The repo to update. If not passed, will try all repos in config/repos.yml", :type => :strings
-  opt :title, "The new milestone title", :type => :string, :required => true
+  opt :repo,   "The repo to update. If not passed, will try all repos in config/repos.yml", :type => :strings
+
+  opt :title,  "The new milestone title",    :type => :string, :required => true
+  opt :due_on, "The new milestone due date", :type => :string, :required => true
+
   opt :dry_run, "", :default => false
 end
 
+Optimist.die(:due_on, "must be a date format") unless ManageIQ::Release::ReleaseMilestone.valid_date?(opts[:due_on])
+
 ManageIQ::Release.each_repo(opts[:repo]) do |repo|
-  ManageIQ::Release::ReleaseMilestone.new(repo, opts.slice(:title, :dry_run)).run
+  ManageIQ::Release::ReleaseMilestone.new(repo, opts.slice(:title, :due_on, :dry_run)).run
 end
