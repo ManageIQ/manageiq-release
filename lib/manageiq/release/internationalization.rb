1844463642bf9349857bb27d9a0354ec7c82bfbd
Oleg Barenboim <chessbyte@gmail.com>
2020-08-18 14:26:43 -0400

    Add Service UI automation for generating message catalog

--- lib/manageiq/release/internationalization.rb
+++ lib/manageiq/release/internationalization.rb
@@ -14,6 +14,7 @@ module ManageIQ
       # If there are material differences, push up to Transifex
       def update_message_catalogs
         generate_message_catalog_for_manageiq
+        generate_message_catalog_for_manageiq_service_ui
       end
 
       private
@@ -23,6 +24,15 @@ module ManageIQ
         puts description
         system(command)
       end
+
+      def git_diff_stats(repo)
+        repo.git.capturing.diff(:numstat => true).split("\n").each_with_object({}) do |diff, stats|
+          insertions, deletions, fname = diff.split("\s")
+          stats[fname] = {:insertions => insertions.to_i, :deletions => deletions.to_i }
+        end
+      end
+
+      # ManageIQ Section
 
       def create_database_yml
         puts "*** Creating config/database.yml ***"
@@ -40,7 +50,7 @@ module ManageIQ
 
       def manageiq_message_catalog_file_needs_commit?
         needs_commit = false
-        diffs = git_diff_stats
+        diffs = git_diff_stats(manageiq_repo)
         return false if diffs.length != 1
         return false unless diffs.key?(manageiq_message_catalog_filename)
         stats = diffs[manageiq_message_catalog_filename]
@@ -50,13 +60,6 @@ module ManageIQ
         # then there are no substantive changes
         # Hence why this code checks for precisely 2 insertions and deletions
         (stats[:insertions] != 2) || (stats[:deletions] != 2)
-      end
-
-      def git_diff_stats
-        manageiq_repo.git.capturing.diff(:numstat => true).split("\n").each_with_object({}) do |diff, stats|
-          insertions, deletions, fname = diff.split("\s")
-          stats[fname] = {:insertions => insertions.to_i, :deletions => deletions.to_i }
-        end
       end
 
       def manageiq_repo_path
@@ -79,7 +82,7 @@ module ManageIQ
             execute "bundle exec rake locale:update"
 
             puts "*** Resetting changes to files other than message catalog ***"
-            git_diff_stats.each do |fname, stats|
+            git_diff_stats(manageiq_repo).each do |fname, stats|
               next if fname.end_with?(manageiq_message_catalog_filename)
               puts "- resetting #{fname}"
               system("git checkout HEAD -- #{fname}")
@@ -87,6 +90,40 @@ module ManageIQ
 
             puts "*** Checking #{manageiq_message_catalog_filename} file ***"
             puts "#{manageiq_message_catalog_filename} needs commit?: #{manageiq_message_catalog_file_needs_commit? ? 'YES' : 'NO'}"
+          end
+        end
+      end
+
+      # ManageIQ Service UI Section
+
+      def manageiq_service_ui_repo
+        @manageiq_service_ui_repo ||= Repo.new('ManageIQ/manageiq-ui-service')
+      end
+
+      def manageiq_service_ui_message_catalog_filename
+        "client/gettext/po/manageiq-ui-service.pot"
+      end
+
+      def manageiq_service_ui_message_catalog_file_needs_commit?
+        needs_commit = false
+        diffs = git_diff_stats(manageiq_service_ui_repo)
+        return false if diffs.length != 1
+        return false unless diffs.key?(manageiq_service_ui_message_catalog_filename)
+        stats = diffs[manageiq_service_ui_message_catalog_filename]
+        puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{manageiq_service_ui_message_catalog_filename}"
+        return true
+      end
+
+      def generate_message_catalog_for_manageiq_service_ui
+        Bundler.with_clean_env do
+          manageiq_service_ui_repo.fetch
+          manageiq_service_ui_repo.checkout(branch)
+
+          Dir.chdir(manageiq_service_ui_repo.path) do |dir|
+            execute "yarn install"
+            execute "yarn gettext:extract"
+            puts "*** Checking #{manageiq_service_ui_message_catalog_filename} file ***"
+            puts "#{manageiq_service_ui_message_catalog_filename} needs commit?: #{manageiq_service_ui_message_catalog_file_needs_commit? ? 'YES' : 'NO'}"
           end
         end
       end
