7bc034154ff5c55296161eb3053d12f9a370a236
Oleg Barenboim <chessbyte@gmail.com>
2020-08-28 15:38:45 -0400

    Refactor into classes

--- lib/manageiq/release/internationalization.rb
+++ lib/manageiq/release/internationalization.rb
@@ -9,148 +9,168 @@ module ManageIQ
         @dry_run = dry_run
       end
 
-      # In ManageIQ/manageiq repo, run `rake locale:update`
-      # In ManageIQ/manageiq-ui-service repo, run `gulp gettext-extract`
-      # If there are material differences, push up to Transifex
       def update_message_catalogs
-        update_message_catalog_for_manageiq
-        update_message_catalog_for_manageiq_service_ui
-      end
+        klasses = []
+        klasses << ManageIQ
+        klasses << ManageIQ_ServiceUI
 
-      private
-
-      def execute(command, description = nil)
-        description ||= "*** Running `#{command}` ***"
-        puts description
-        system(command)
-      end
-
-      def git_diff_stats(repo)
-        repo.git.capturing.diff(:numstat => true).split("\n").each_with_object({}) do |diff, stats|
-          insertions, deletions, fname = diff.split("\s")
-          stats[fname] = {:insertions => insertions.to_i, :deletions => deletions.to_i }
+        klasses.each do |klass|
+          klass.new(branch: branch, dry_run: dry_run).update_message_catalog
         end
       end
 
-      # ManageIQ Section
-
-      def create_database_yml
-        puts "*** Creating config/database.yml ***"
-        database_config = YAML.load_file("config/database.pg.yml")
-        unless database_config.key?('i18n')
-          database_config['i18n'] = database_config['test'].dup
-          database_config['i18n']['database'] = "vmdb_i18n"
+      class Helper
+        def execute(command, description = nil)
+          description ||= "*** Running `#{command}` ***"
+          puts description
+          system(command)
         end
-        File.write("config/database.yml", database_config.to_yaml)
-      end
 
-      def manageiq_message_catalog_filename
-        "locale/manageiq.pot"
-      end
-
-      def manageiq_message_catalog_file_needs_commit?
-        needs_commit = false
-        Dir.chdir(manageiq_repo_path) do |dir|
-          diffs = git_diff_stats(manageiq_repo)
-          return false if diffs.length != 1
-          return false unless diffs.key?(manageiq_message_catalog_filename)
-          stats = diffs[manageiq_message_catalog_filename]
-          puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{manageiq_message_catalog_filename}"
-
-          # If the only changes are to POT-Creation-Date and PO-Revision-Date,
-          # then there are no substantive changes
-          # Hence why this code checks for precisely 2 insertions and deletions
-          needs_commit = (stats[:insertions] != 2) || (stats[:deletions] != 2)
+        def git_diff_stats(repo)
+          repo.git.capturing.diff(:numstat => true).split("\n").each_with_object({}) do |diff, stats|
+            insertions, deletions, fname = diff.split("\s")
+            stats[fname] = {:insertions => insertions.to_i, :deletions => deletions.to_i }
+          end
         end
-        puts "#{manageiq_message_catalog_filename} needs commit?: #{needs_commit ? 'YES' : 'NO'}"
-        needs_commit
       end
 
-      def manageiq_repo_path
-        manageiq_repo.path || File.join(__dir__, "../../../repos/ManageIQ/manageiq")
-      end
+      class ManageIQ
+        attr_reader :branch, :dry_run, :helper
 
-      def manageiq_repo
-        @manageiq_repo ||= Repo.new('ManageIQ/manageiq')
-      end
+        def initialize(branch: nil, dry_run: true)
+          @branch  = branch || 'master'
+          @dry_run = dry_run
+          @helper  = Helper.new
+        end
 
-      def generate_message_catalog_for_manageiq
-        Bundler.with_clean_env do
-          manageiq_repo.fetch
-          manageiq_repo.checkout(branch)
+        def create_database_yml
+          puts "*** Creating config/database.yml ***"
+          database_config = YAML.load_file("config/database.pg.yml")
+          unless database_config.key?('i18n')
+            database_config['i18n'] = database_config['test'].dup
+            database_config['i18n']['database'] = "vmdb_i18n"
+          end
+          File.write("config/database.yml", database_config.to_yaml)
+        end
 
-          Dir.chdir(manageiq_repo_path) do |dir|
-            create_database_yml
+        def message_catalog_filename
+          "locale/manageiq.pot"
+        end
 
-            execute "RAILS_ENV=i18n SKIP_TEST_RESET=true bin/setup"
-            execute "bundle exec rake locale:update"
+        def message_catalog_file_needs_commit?
+          needs_commit = false
+          Dir.chdir(repo_path) do |dir|
+            diffs = helper.git_diff_stats(repo)
+            return false if diffs.length != 1
+            return false unless diffs.key?(message_catalog_filename)
+            stats = diffs[manageiq_message_catalog_filename]
+            puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
 
-            puts "*** Resetting changes to files other than message catalog ***"
-            git_diff_stats(manageiq_repo).each do |fname, stats|
-              next if fname.end_with?(manageiq_message_catalog_filename)
-              puts "- resetting #{fname}"
-              system("git checkout HEAD -- #{fname}")
+            # If the only changes are to POT-Creation-Date and PO-Revision-Date,
+            # then there are no substantive changes
+            # Hence why this code checks for precisely 2 insertions and deletions
+            needs_commit = (stats[:insertions] != 2) || (stats[:deletions] != 2)
+          end
+          puts "#{message_catalog_filename} needs commit?: #{needs_commit ? 'YES' : 'NO'}"
+          needs_commit
+        end
+
+        def repo_path
+          repo.path || File.join(__dir__, "../../../repos/ManageIQ/manageiq")
+        end
+
+        def repo
+          @repo ||= Repo.new('ManageIQ/manageiq')
+        end
+
+        def generate_message_catalog
+          Bundler.with_clean_env do
+            repo.fetch
+            repo.checkout(branch)
+
+            Dir.chdir(repo_path) do |dir|
+              create_database_yml
+
+              helper.execute "RAILS_ENV=i18n SKIP_TEST_RESET=true bin/setup"
+              helper.execute "bundle exec rake locale:update"
+
+              puts "*** Resetting changes to files other than message catalog ***"
+              helper.git_diff_stats(repo).each do |fname, stats|
+                next if fname.end_with?(message_catalog_filename)
+                puts "- resetting #{fname}"
+                system("git checkout HEAD -- #{fname}")
+              end
             end
           end
         end
-      end
 
-      def upload_message_catalog_for_manageiq
-        puts "Uploading #{manageiq_message_catalog_filename} to Transifex"
-        Dir.chdir(manageiq_repo_path) do |dir|
-          system("tx push --source")
-        end
-      end
-
-      def update_message_catalog_for_manageiq
-        generate_message_catalog_for_manageiq
-        upload_message_catalog_for_manageiq
-      end
-
-      # ManageIQ Service UI Section
-
-      def manageiq_service_ui_repo
-        @manageiq_service_ui_repo ||= Repo.new('ManageIQ/manageiq-ui-service')
-      end
-
-      def manageiq_service_ui_message_catalog_filename
-        "client/gettext/po/manageiq-ui-service.pot"
-      end
-
-      def manageiq_service_ui_message_catalog_file_needs_commit?
-        Dir.chdir(manageiq_service_ui_repo.path) do |dir|
-          diffs = git_diff_stats(manageiq_service_ui_repo)
-          return false if diffs.length != 1
-          return false unless diffs.key?(manageiq_service_ui_message_catalog_filename)
-          stats = diffs[manageiq_service_ui_message_catalog_filename]
-          puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{manageiq_service_ui_message_catalog_filename}"
-          return true
-        end
-      end
-
-      def generate_message_catalog_for_manageiq_service_ui
-        Bundler.with_clean_env do
-          manageiq_service_ui_repo.fetch
-          manageiq_service_ui_repo.checkout(branch)
-
-          Dir.chdir(manageiq_service_ui_repo.path) do |dir|
-            execute "yarn install"
-            execute "yarn gettext:extract"
+        def upload_message_catalog
+          puts "Uploading #{message_catalog_filename} to Transifex"
+          Dir.chdir(repo_path) do |dir|
+            system("tx push --source")
           end
         end
-      end
 
-      def upload_message_catalog_for_manageiq_service_ui
-        puts "Uploading #{manageiq_service_ui_message_catalog_filename} to Transifex"
-        Dir.chdir(manageiq_service_ui_repo.path) do |dir|
-          system("tx push --source")
+        def update_message_catalog
+          generate_message_catalog
+          upload_message_catalog
         end
       end
 
-      def update_message_catalog_for_manageiq_service_ui
-        generate_message_catalog_for_manageiq_service_ui
-        upload_message_catalog_for_manageiq_service_ui
+      class ManageIQ_ServiceUI
+        attr_reader :branch, :dry_run, :helper
+
+        def initialize(branch: nil, dry_run: true)
+          @branch  = branch || 'master'
+          @dry_run = dry_run
+          @helper  = Helper.new
+        end
+
+        def repo
+          @repo ||= Repo.new('ManageIQ/manageiq-ui-service')
+        end
+
+        def message_catalog_filename
+          "client/gettext/po/manageiq-ui-service.pot"
+        end
+
+        def message_catalog_file_needs_commit?
+          Dir.chdir(repo.path) do |dir|
+            diffs = helper.git_diff_stats(repo)
+            return false if diffs.length != 1
+            return false unless diffs.key?(message_catalog_filename)
+            stats = diffs[message_catalog_filename]
+            puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
+            return true
+          end
+        end
+
+        def generate_message_catalog
+          Bundler.with_clean_env do
+            repo.fetch
+            repo.checkout(branch)
+
+            Dir.chdir(repo.path) do |dir|
+              helper.execute "yarn install"
+              helper.execute "yarn gettext:extract"
+            end
+          end
+        end
+
+        def upload_message_catalog
+          puts "Uploading #{message_catalog_filename} to Transifex"
+          Dir.chdir(repo.path) do |dir|
+            system("tx push --source")
+          end
+        end
+
+        def update_message_catalog
+          generate_message_catalog
+          upload_message_catalog
+        end
+
       end
+
     end
   end
 end
