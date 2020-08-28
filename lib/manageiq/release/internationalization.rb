88ae976eee2df2c06cabdf71c616e281b03e5fa3
Oleg Barenboim <chessbyte@gmail.com>
2020-08-28 16:27:18 -0400

    Added auto-creation of .tx/config, if missing

--- lib/manageiq/release/internationalization.rb
+++ lib/manageiq/release/internationalization.rb
@@ -32,6 +32,19 @@ module ManageIQ
             stats[fname] = {:insertions => insertions.to_i, :deletions => deletions.to_i }
           end
         end
+
+        def create_tx_config(tx_config_content)
+          return if File.exist?(".tx/config")
+          puts "*** Creating .tx/config ***"
+          Dir.mkdir(".tx") unless Dir.exist?(".tx")
+          File.write(".tx/config", tx_config_content)
+        end
+
+        def with_checked_out_repo(repo, branch)
+          repo.fetch
+          repo.checkout(branch)
+          repo.chdir { yield }
+        end
       end
 
       class ManageIQ
@@ -41,6 +54,44 @@ module ManageIQ
           @branch  = branch || 'master'
           @dry_run = dry_run
           @helper  = Helper.new
+        end
+
+        def repo
+          @repo ||= Repo.new('ManageIQ/manageiq')
+        end
+
+        def tx_config_content
+          <<~CONTENT
+            [main]
+            host = https://www.transifex.com
+
+            [manageiq.manageiq-#{branch}]
+            file_filter = locale/<lang>/manageiq.po
+            source_file = #{message_catalog_filename}
+            source_lang = en
+            type = PO
+          CONTENT
+        end
+
+        def message_catalog_filename
+          "locale/manageiq.pot"
+        end
+
+        def message_catalog_file_needs_commit?
+          needs_commit = false
+          diffs = helper.git_diff_stats(repo)
+          return false if diffs.length != 1
+          return false unless diffs.key?(message_catalog_filename)
+          stats = diffs[manageiq_message_catalog_filename]
+          puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
+
+          # If the only changes are to POT-Creation-Date and PO-Revision-Date,
+          # then there are no substantive changes
+          # Hence why this code checks for precisely 2 insertions and deletions
+          needs_commit = (stats[:insertions] != 2) || (stats[:deletions] != 2)
+
+          puts "#{message_catalog_filename} needs commit?: #{needs_commit ? 'YES' : 'NO'}"
+          needs_commit
         end
 
         def create_database_yml
@@ -53,81 +104,63 @@ module ManageIQ
           File.write("config/database.yml", database_config.to_yaml)
         end
 
-        def message_catalog_filename
-          "locale/manageiq.pot"
-        end
-
-        def message_catalog_file_needs_commit?
-          needs_commit = false
-          Dir.chdir(repo_path) do |dir|
-            diffs = helper.git_diff_stats(repo)
-            return false if diffs.length != 1
-            return false unless diffs.key?(message_catalog_filename)
-            stats = diffs[manageiq_message_catalog_filename]
-            puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
-
-            # If the only changes are to POT-Creation-Date and PO-Revision-Date,
-            # then there are no substantive changes
-            # Hence why this code checks for precisely 2 insertions and deletions
-            needs_commit = (stats[:insertions] != 2) || (stats[:deletions] != 2)
-          end
-          puts "#{message_catalog_filename} needs commit?: #{needs_commit ? 'YES' : 'NO'}"
-          needs_commit
-        end
-
-        def repo_path
-          repo.path || File.join(__dir__, "../../../repos/ManageIQ/manageiq")
-        end
-
-        def repo
-          @repo ||= Repo.new('ManageIQ/manageiq')
-        end
-
         def generate_message_catalog
           Bundler.with_clean_env do
-            repo.fetch
-            repo.checkout(branch)
+            create_database_yml
 
-            Dir.chdir(repo_path) do |dir|
-              create_database_yml
+            helper.execute "RAILS_ENV=i18n SKIP_TEST_RESET=true bin/setup"
+            helper.execute "bundle exec rake locale:update"
 
-              helper.execute "RAILS_ENV=i18n SKIP_TEST_RESET=true bin/setup"
-              helper.execute "bundle exec rake locale:update"
-
-              puts "*** Resetting changes to files other than message catalog ***"
-              helper.git_diff_stats(repo).each do |fname, stats|
-                next if fname.end_with?(message_catalog_filename)
-                puts "- resetting #{fname}"
-                system("git checkout HEAD -- #{fname}")
-              end
+            puts "*** Resetting changes to files other than message catalog ***"
+            helper.git_diff_stats(repo).each do |fname, stats|
+              next if fname.end_with?(message_catalog_filename)
+              puts "- resetting #{fname}"
+              system("git checkout HEAD -- #{fname}")
             end
           end
         end
 
         def upload_message_catalog
           puts "Uploading #{message_catalog_filename} to Transifex"
-          Dir.chdir(repo_path) do |dir|
-            system("tx push --source")
-          end
+          helper.create_tx_config(tx_config_content)
+          system("tx push --source")
         end
 
         def update_message_catalog
-          generate_message_catalog
-          upload_message_catalog
+          helper.with_checked_out_repo(repo, branch) do
+            generate_message_catalog
+            upload_message_catalog
+          end
         end
       end
 
       class ManageIQ_ServiceUI
-        attr_reader :branch, :dry_run, :helper
+        attr_reader :branch, :dry_run
 
         def initialize(branch: nil, dry_run: true)
           @branch  = branch || 'master'
           @dry_run = dry_run
-          @helper  = Helper.new
+        end
+
+        def helper
+          @helper ||= Helper.new
         end
 
         def repo
           @repo ||= Repo.new('ManageIQ/manageiq-ui-service')
+        end
+
+        def tx_config_content
+          <<~CONTENT
+            [main]
+            host = https://www.transifex.com
+
+            [manageiq-service-ui.manageiq-service-ui-#{branch}]
+            file_filter = client/gettext/po/<lang>/manageiq-ui-service.po
+            source_file = #{message_catalog_filename}
+            source_lang = en
+            type = PO
+          CONTENT
         end
 
         def message_catalog_filename
@@ -135,42 +168,32 @@ module ManageIQ
         end
 
         def message_catalog_file_needs_commit?
-          Dir.chdir(repo.path) do |dir|
-            diffs = helper.git_diff_stats(repo)
-            return false if diffs.length != 1
-            return false unless diffs.key?(message_catalog_filename)
-            stats = diffs[message_catalog_filename]
-            puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
-            return true
-          end
+          diffs = helper.git_diff_stats(repo)
+          return false if diffs.length != 1
+          return false unless diffs.key?(message_catalog_filename)
+          stats = diffs[message_catalog_filename]
+          puts "+#{stats[:insertions]}, -#{stats[:deletions]} for #{message_catalog_filename}"
+          return true
         end
 
         def generate_message_catalog
-          Bundler.with_clean_env do
-            repo.fetch
-            repo.checkout(branch)
-
-            Dir.chdir(repo.path) do |dir|
-              helper.execute "yarn install"
-              helper.execute "yarn gettext:extract"
-            end
-          end
+          helper.execute "yarn install"
+          helper.execute "yarn gettext:extract"
         end
 
         def upload_message_catalog
           puts "Uploading #{message_catalog_filename} to Transifex"
-          Dir.chdir(repo.path) do |dir|
-            system("tx push --source")
-          end
+          helper.create_tx_config(tx_config_content)
+          system("tx push --source")
         end
 
         def update_message_catalog
-          generate_message_catalog
-          upload_message_catalog
+          helper.with_checked_out_repo(repo, branch) do
+            generate_message_catalog
+            upload_message_catalog
+          end
         end
-
       end
-
     end
   end
 end
