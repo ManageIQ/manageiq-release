511de967a008e84b611f9cf4ed80a19d361c85b4
Jason Frey <fryguy9@gmail.com>
2022-09-07 11:12:53 -0400

    Add ability to skip repos in each_repo

--- bin/each_repo.rb
+++ bin/each_repo.rb
@@ -8,14 +8,23 @@ require 'optimist'
 
 opts = Optimist.options do
   opt :command, "A command to run in each repo", :type => :string, :required => true
-  opt :ref, "Ref to checkout before running the command", :type => :string, :default => "master"
+  opt :ref,     "Ref to checkout before running the command", :type => :string, :default => "master"
+
+  opt :skip,    "The repos to skip", :type => :strings, :default => []
 
   ManageIQ::Release.common_options(self, :except => :dry_run)
 end
 
 ManageIQ::Release.each_repo(**opts) do |r|
-  r.fetch
-  r.checkout(opts[:ref])
+  next if opts[:skip].include?(r.github_repo)
+
+  begin
+    r.fetch
+    r.checkout(opts[:ref])
+  rescue MiniGit::GitError
+    next
+  end
+
   r.chdir do
     puts "+ #{opts[:command]}"
     system(opts[:command])
