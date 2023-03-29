13d64cf7d2a5188ec8e3d32d069bca59104ad7ad
Jason Frey <fryguy9@gmail.com>
2023-03-29 12:46:30 -0400

    Remove files extracted into multi_repo gem

--- bin/github_rate_limit.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-$LOAD_PATH << File.expand_path("../lib", __dir__)
-
-require 'bundler/setup'
-require 'manageiq/release'
-
-puts [ManageIQ::Release.github.rate_limit.to_h].tableize
