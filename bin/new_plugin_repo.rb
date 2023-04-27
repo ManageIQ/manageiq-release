5bfb979187d0b793a731a4250bba636cd8bda074
Jason Frey <fryguy9@gmail.com>
2023-04-27 17:00:39 -0400

    Update new_plugin_repo to use multi_repo gem

--- bin/new_plugin_repo.rb
+++ bin/new_plugin_repo.rb
@@ -1,40 +1,40 @@
 #!/usr/bin/env ruby
 
-$LOAD_PATH << File.expand_path("../lib", __dir__)
-
-require 'bundler/setup'
-require 'manageiq/release'
-require 'optimist'
+require "bundler/inline"
+gemfile do
+  source "https://rubygems.org"
+  gem "multi_repo", require: "multi_repo/cli", path: File.expand_path("~/dev/multi_repo")
+end
 
 opts = Optimist.options do
   opt :plugin, "Whether or not this is a ManageIQ plugin", :type => :boolean, :required => true
 
-  ManageIQ::Release.common_options(self, :except => :repo_set)
+  MultiRepo::CLI.common_options(self, :except => :repo_set)
 end
 
 repo_name = opts[:repo].first
-repo = ManageIQ::Release.repo_for(repo_name)
+repo = MultiRepo::CLI.repo_for(repo_name)
 
-has_repo = !opts[:plugin] || ManageIQ::Release::RepoSet.config["master"].include?(repo_name)
-has_labels = ManageIQ::Release::Labels.config["repos"][repo.github_repo]
+has_repo = !opts[:plugin] || MultiRepo::RepoSet.config["master"].include?(repo_name)
+has_labels = MultiRepo::Labels.config["repos"][repo.name]
 unless has_repo && has_labels
   STDERR.puts "ERROR: First update config/repos.yml and config/labels.yml with the new repo"
   exit 1
 end
 
 puts "\n** Updating Repo Settings"
-ManageIQ::Release::UpdateRepoSettings.new(repo.github_repo, opts).run
+MultiRepo::Helper::UpdateRepoSettings.new(repo.name, **opts).run
 puts "\n** Updating Branch Protection"
-ManageIQ::Release::UpdateBranchProtection.new(repo.github_repo, opts.merge(:branch => "master")).run
+MultiRepo::Helper::UpdateBranchProtection.new(repo.name, **opts.merge(:branch => "master")).run
 puts "\n** Updating Labels"
-ManageIQ::Release::UpdateLabels.new(repo.github_repo, opts).run
+MultiRepo::Helper::UpdateLabels.new(repo.name, **opts).run
 puts "\n** Reserve rubygems entry"
 if opts[:plugin]
-  ManageIQ::Release::RubygemsStub.new(repo.name, opts).run
+  MultiRepo::Helper::RubygemsStub.new(repo.name, **opts).run
 end
 
 puts "\n** Preparing Pull Request"
-ManageIQ::Release::PullRequestBlasterOuter.new(repo, opts.merge(
+MultiRepo::Helper::PullRequestBlasterOuter.new(repo, **opts.merge(
   :base    => "master",
   :head    => "new_plugin_repo",
   :script  => "scripts/pull_request_blaster_outer/new_plugin_repo.rb",
@@ -45,7 +45,7 @@ puts
 puts "******* MANUAL THINGS *******"
 puts "- Add repo to repos.sets.yml if this is a new core or provider plugin"
 puts "- Add repo to mirror settings"
-puts "- https://codeclimate.com/github/#{repo.github_repo} => Repo Settings => GitHub => Pull Request Status Updates => Install"
+puts "- https://codeclimate.com/github/#{repo.name} => Repo Settings => GitHub => Pull Request Status Updates => Install"
 puts "  - Ensure that CC_TEST_REPORTER_ID is set in the repo settings"
 puts "- https://gitter.im/ManageIQ#createroom and create a new room linked to the repository"
 puts "- Add repo to the bot"
